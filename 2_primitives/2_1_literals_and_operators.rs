da9dd550eb081c8d3487cb641b9c081d0d24dcbe
JestVA <d.dumitrascuta@gmail.com>
2023-03-02 07:57:30 +0000

    literals and operators

---
+++ 2_primitives/2_1_literals_and_operators.rs
@@ -0,0 +1,19 @@
+fn main() {
+    // integer addition
+    println!("1 + 2 = {}", 1u32 + 2);
+    // integer subtraction
+    println!("1 - 2 = {}", 1i32 - 2);
+    // short circuiting boolean logic
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+    // bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    //use underscores to improve readability
+    println!("One million is written as {}", 1_000_000u32);
+}
